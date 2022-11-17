aec17eb1ce3352e3d14ba1236628e232f85e73b9
JoshuaWeissTBS <joshua.weiss@hotmail.com>
2022-11-17 10:57:52 -0800

    made cube spin

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import { Canvas } from '@react-three/fiber'
-import reactLogo from './assets/react.svg'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import { Mesh } from 'three'
 import './App.css'
 
 function ThreeScene() {
+  const boxRef = useRef<Mesh>(null!)
+
+  useFrame(() => {
+    if (boxRef.current) {
+      boxRef.current.rotation.x += 0.01
+      boxRef.current.rotation.y += 0.01
+    }
+  })
+
   return (
-    <Canvas>
-      <ambientLight />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color='orange' />
-      </mesh>
-    </Canvas>
+    <mesh ref={boxRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color='orange' />
+    </mesh>
   )
 };
 
 function App() {
   return (
     <div className='App h-screen'>
-      <ThreeScene/>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[5,5,5]} />
+        <ThreeScene/>
+      </Canvas>
     </div>
   )
 }
