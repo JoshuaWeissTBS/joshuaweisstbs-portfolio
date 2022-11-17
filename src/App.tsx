86334f2d218397638166279128d4a43d19f7c1ec
JoshuaWeissTBS <joshua.weiss@hotmail.com>
2022-11-17 08:10:00 -0800

    fixed project to work with three js, updated project name, added square

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import { Canvas } from '@react-three/fiber'
+import reactLogo from './assets/react.svg'
 import './App.css'
+
+function ThreeScene() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color='orange' />
+      </mesh>
+    </Canvas>
+  )
+};
 
 function App() {
   return (
-    <>
-    </>
+    <div className='App h-screen'>
+      <ThreeScene/>
+    </div>
   )
 }
 
